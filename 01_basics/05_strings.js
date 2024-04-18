5404e38272867ebbdedc73326916cada7288c3e4
SHUBham66ingh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-18 21:57:01 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+const name="hitesh"
+const repoCount= 50
+
+// console.log(name+repocount+"value");
+console.log(`hello my name is ${name} and my repo is ${repoCount}`);
+
+const gameName= new String('shubham')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase())
