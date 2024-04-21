73525310dcaa7b9b55560a25b6255ddd8367df44
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-21 17:05:09 +0000

    ..

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+// singleton
+
+// object literals
+Object.create
+const JsUser= {
+    name: "hitesh",
+    age:18,
+    location:"jaipur",
+    email:"shubham1041google.com",
+    isLoggedIn : false,
+    lastLoginDays: ["Monady" ,"saturday" ,"wednesday"]
+}
+
+console.log(JsUser.email)
+
+
