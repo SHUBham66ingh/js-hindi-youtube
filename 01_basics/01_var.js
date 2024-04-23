d0c2a37620503eb0b21677db72bb85644cfdd3f2
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-23 22:50:23 +0000

    first change

---
+++ 01_basics/01_var.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail="hits@gmail.com"
+var accountPassword ="12345"
+accoutnCity="jaipur"
+
+//accountId=2;// not allowed
+
+accountEmail = "shubham@gmail.com"
+accountPassword= "124677"
+accoutnCity="bengaluru"
+
+console.log(accountId);
+
+console.log([accountId])
---
+++ 02_basics/05_objects.js
@@ -0,0 +1,23 @@
+const course = {
+    coursename: "js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+//course.courseInstructor
+
+const {courseInstructor : Instructor}=  course
+
+console.log(courseInstructor);
+console.log(Instructor);
+
+//{
+   // "name": "hitseh",
+  //  'coursename": "js in hindi",
+  //  "price":"free"
+//}
+
+[
+    {},
+    {},
+    {}
+]
