b6782e93187e834ab47f63383707a7a09e69ca5e
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-24 22:48:32 +0000

    conversion

---
+++ 01_basics/03_datatypesConversion.js
@@ -0,0 +1,31 @@
+let score ="33abc";
+
+console.log(typeof score);//string
+console.log(typeof (score));//string
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);// number
+console.log(valueInNumber); // NaN
+
+
+// "33" => 33
+// "33abc" => NaN
+// true= 1 false=0
+ let isLoggedIn= 1;
+ let boolIsloggedIn = Boolean(isLoggedIn);
+ console.log(boolIsloggedIn);
+
+ // 1=> true  0=> false;
+ // ""=> false;
+ // "hitesh" => true
+
+ let someNumber = 33
+ let stringNumber=  String(someNumber);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+ 
+ 
+ 
+
+
+
