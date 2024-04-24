83e767be1bb9cc7cdbf17932a198c11613d5a3f7
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-24 23:04:19 +0000

    operations

--- 01_basics/03_datatypesConversion.js
+++ 01_basics/03_datatypesConversion.js
@@ -23,6 +23,40 @@ console.log(valueInNumber); // NaN
  let stringNumber=  String(someNumber);
  console.log(stringNumber);
  console.log(typeof stringNumber);
+
+
+ //**************operations*****************//
+
+ let value =3;
+ let negValue = -value;
+ console.log(negValue);
+
+
+ let str1= "hello"
+ let str2= "shubham"
+
+ let str3= str1+str2;
+ console.log(str3);// cocatenation of the strings.....
+
+ console.log("1"+2);//12
+ console.log(1+"2");//12
+ console.log("1"+2+2);//122
+ console.log(1+2+"2");//32
+
+ console.log(+true) // 1
+ console.log(+" "); // 0
+
+ let num1,num2,num3;
+ num1=num2=num3= 2+2;
+
+ let gameCouner=100;
+ gameCounter++;
+ console.log(gameCouner); // 101
+
+
+
+
+
  
  
  
