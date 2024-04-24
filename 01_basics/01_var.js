94e09b65a5e7cca13a2ef95e70b15ae7667e7bc0
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-24 21:15:37 +0000

    first

--- 01_basics/01_var.js
+++ 01_basics/01_var.js
@@ -2,13 +2,22 @@ const accountId = 14453
 let accountEmail="hits@gmail.com"
 var accountPassword ="12345"
 accoutnCity="jaipur"
+let accountState = " ";
 
 //accountId=2;// not allowed
-
+accountId=12355;
 accountEmail = "shubham@gmail.com"
 accountPassword= "124677"
 accoutnCity="bengaluru"
 
-console.log(accountId);
+/*
+prefer not to use var
+because of issue in block scope and fuctional scope
+*/
 
-console.log([accountId])
+
+console.log(accountId);
+console.log([accountId]);
+
+console.table([accountId,accountEmail,accountPassword,accoutnCity,accountState]);
+
