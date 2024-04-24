32b4b801e39fa20e7bcf06ea0ee1a354393cd874
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-24 21:31:31 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all js code as newer version...
+
+//alert(3+3)// we are using node js not browser
+
+console.log(3+3); 
+console.log("shubham")
+
+let name ="shubham";
+let age=18
+let isLoggeIn=false;
+let state = null;
+
+
+// number=> 2 to power 53
+// bignit
+// string
+// boolean true/false
+//null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);// object;
+console.log(typeof undefined);// underfined;
