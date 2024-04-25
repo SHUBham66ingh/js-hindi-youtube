8ea1c7439b36666e250863f3599e9b1574219b67
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-25 23:20:20 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,4 +31,40 @@ function loginUserMessage(username)
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("shubham"));
+//console.log(loginUserMessage("shubham"));
+
+
+function calculateCartPrice(val1,val2,...num1)
+{
+  return num1;
+}
+
+//console.log(calculateCartPrice(200,400,500,300));
+const user={
+    username:"shubham",
+    price:100
+}
+
+function handleObject(anyobject)
+{
+console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+};
+
+handleObject(user);//
+
+handleObject(
+    {
+        username:"shubh",
+        price:1000
+    }
+)
+
+const myNewArray = [200,400,500,600]
+
+function returnSecondValue(getArray)
+{
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+
