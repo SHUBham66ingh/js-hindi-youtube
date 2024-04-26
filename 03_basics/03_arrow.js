6c834e6b91b91f96fd2eaa747ca8f54224320e69
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-26 11:20:21 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,50 +1,54 @@
-//const user={
-   // username:"shubham",
- //   price:100,
-  //  welcomeMessage : function()
-  //  {
-   //     console.log(`${this.username}, wleocome to website`);
-   //     console.log(this)
-   // }  
-//}
+const user = {
+  username: "hitesh",
+  price: 999,
 
-//user.welcomeMessage();
-//user.username = "sam";
-//user.welcomeMessage();
+  welcomeMessage: function() {
+      console.log(`${this.username} , welcome to website`);
+      console.log(this);
+  }
 
-//console.log(this);
-
-//function chai()
-//{
-  //  let username = "shubham"// underdefined
-   // console.log(this);
-//}
-//chai();
-
-//const chai = function()
-//{
- //   let username = "shubham"// underdefined
-  //  console.log(this);
-//}
-
-
-//const chai = function()
-//{
-   // let username = "shubham"// underdefined
-  //  console.log(this.username);
-//}
-//chai();
-
-
-//const chai = ()=>
-{
-  //  let username = "shubham"// underdefined
-   // console.log(this.username);
-//}
-//chai();
-
-const  addTwo = (num1,num2) => {
-    return num1+num2
 }
 
-console.log( addTwo(1,2) );
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+  let username = "hitesh"
+  console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => num1+num2
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
