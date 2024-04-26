1eb325be4cd202d349770012216c8e5cdf75a890
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-26 21:37:38 +0000

    one

--- 05_control_flow/truthy.js
+++ 05_control_flow/truthy.js
@@ -7,3 +7,15 @@ else
 {
  console.log("dont have user email");
 }
+// false,0,-0,bigint 0n," ",null , undefined,NaN
+// true,"0",1,"false"," ",[],{},function(){}
+if(userEmail===0)
+{
+    console.log("array is empty")
+}
+const emptyObj={}
+
+    if(Object.keys(emtyObj).lenght===0)
+    {
+      console.log("obj is empty");
+    }
