f731f63eac11c275343dcb51af2d207e3747ee3b
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-26 19:19:31 +0000

    one

---
+++ 05_control_flow/one.js
@@ -0,0 +1,11 @@
+// if
+const isUserLoggedIn = true;
+const temperature=41;
+if (temperature<50)
+{
+console.log(" temperature less than 50");
+}
+else
+{
+console.log(" emperaturegreater than 50")
+}
