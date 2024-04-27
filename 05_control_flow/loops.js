2f81d20a454d13c153f7d5c49981a98c15c993e0
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-27 20:10:21 +0000

    loops

--- 05_control_flow/loops.js
+++ 05_control_flow/loops.js
@@ -1,6 +1,26 @@
 // for
 for (let i = 0; i <= 10; i++) {
-    const element = index;
+    const element = i;
+    if(element==5)
+    {
+   // console.log("5 is best number");
+}
+//console.log(element);
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`outer loop value:${i}`)
+    for (let j = 1; j <=10 ; j++) {
+    console.log(`inner loop ${j} and ${i}`);
+    console.log(i + '*' + j + '=' +  i*j) 
+    }  
+}
+
+let myArray = ["flash","batman","superman"];
+for(let i=0;i < myArray.length;i++)
+{
+    const element= myArray[i];
     console.log(element);
 }
 
+//
