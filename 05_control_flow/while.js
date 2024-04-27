fc872420916d5b3860e5d4378d85ee9f05ed1471
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-27 21:31:03 +0000

    while

---
+++ 05_control_flow/while.js
@@ -0,0 +1,21 @@
+let index=0
+
+while(index<=10)
+{
+console.log(`value of index is ${index}`);
+index=index+2;
+}
+
+let myArray=['flash','batman','superman'];
+let i=0;
+while(i<3)
+{
+    console.log(myArray[i]);
+    i=i+1;
+}
+let score=1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
