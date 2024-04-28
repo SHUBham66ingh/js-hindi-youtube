8fa517f7a25cac27accd6ce2ba2d6e9a8e9e1c5b
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-28 11:22:56 +0000

    loop

--- 05_control_flow/6.js
+++ 05_control_flow/6.js
@@ -9,7 +9,29 @@
 
 const myNums=[1,2,3,4,5,6,7,8,9,10]
 
-const newNums = myNums.filter( (num)=> {
-      return num>4  })
+// const newNums = myNums.filter( (num)=> {
+//       return num>4  })
+
+const newNums=[]
+
+myNums.forEach((num)=> 
+{
+    if(num>4)
+    {
+        newNums.push(num)
+    }
+
+})
 console.log(newNums);
 
+const books=[
+    {title:'book one',genre:'fiction',publish:1981,edition:2004},
+    {title:'book two',genre:'Non-fiction',publish:1981,edition:2004},
+    {title:'book three',genre:'history',publish:2004,edition:2004},
+    {title:'book four',genre:'science',publish:1981,edition:2004},
+];
+
+const userBooks = books.filter((bk) => bk.genre==='history')
+userBooks = books.filter((bk) => {bk.publish>2000})
+
+console.log(userBooks);
