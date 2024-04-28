2a0722d348a8df4a481082c27620f309693bcc6b
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-04-28 10:24:52 +0000

    loops

---
+++ 05_control_flow/6.js
@@ -0,0 +1,14 @@
+// const coding=["js","ruby","java","cpp","python"];
+
+// const value = coding.forEach((item)=>
+// {
+//   console.log(item);
+// })
+
+// console.log(value);
+
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+
+const newNums = myNums.filter( (num)=> num>4 )
+console.log(newNums);
+
