c0b2255c11f38a5d47feff5a0eecaa8ddbac60df
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-05-04 10:44:10 +0000

    promise

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,2 +1,13 @@
 // fetch('https://something.com').then().catch().finally()
-const promiseOne= new Promise(function(resolve,reject))
+const promiseOne= new Promise(function(resolve,reject){
+// do any async task
+//  db calls, CRYPTOgraphically, network
+setTimneout(function()
+{
+  console.log('async task is complete')
+},1000)
+})
+
+promiseOne.then(function(){
+    console.log("promise consumed");
+})
