5882962957a79ea1b43c402a221fbbb431215688
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-05-04 18:54:01 +0000

    6

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,10 +1,10 @@
-//same datatype....
+//same datatype>>>>>
 //console.log(2>1);
 //console.log(2>=1);
 //console.log(2<1);
 //console.log(2==1);
 
-
+// not same data type>>>>>
 console.log("2">1); // true
 console.log("02">1); // true
 // avoid this types of conversion.....
@@ -12,8 +12,12 @@ console.log(null>0);//false
 console.log(null==0);//false
 console.log(null>=0);//true
 
-console.log(undefined==0)
-console.log(undefined>0)
-console.log(undefined<0)
+console.log(undefined==0) //false
+console.log(undefined>0) //false
+console.log(undefined<0) //false
 
+// '===' this checks data and datatype>>>>>>>>>
 console.log("2"===2);
+
+
+// done>>>>>>>>>>>>>>>>>>
