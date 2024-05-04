ba748146c2815b501b46745116be7b416ab7e7eb
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-05-04 18:26:42 +0000

    2

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,24 +3,28 @@
 //alert(3+3)// we are using node js not browser
 
 console.log(3+3); 
-console.log("shubham")
+console.log("shubham");// code  should be readable....
 
-let name ="shubham";
-let age=18
-let isLoggeIn=false;
+let name ="shubham";// string
+let age=18// number
+let isLoggeIn=false; // boolean
 let state = null;
 
 
 // number=> 2 to power 53
-// bignit
-// string
+// bignit //for large  numbers
+// string " " / '  '
 // boolean true/false
 //null => standalone value
 // undefined
-// symbol => unique
+// symbol => to find unique variables
 
 
 // object
 
 console.log(typeof null);// object;
 console.log(typeof undefined);// underfined;
+
+
+
+// done>>>>>>>
