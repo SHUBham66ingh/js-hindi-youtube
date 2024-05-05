dc95c50a409ea819cb54550b0c52ae61a8994c2d
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-05-05 10:44:21 +0000

    date

--- 01_basics/07_date_and_time.js
+++ 01_basics/07_date_and_time.js
@@ -1,15 +1,19 @@
+
+// dates
 let myDate= new Date;
 //console.log(myDate.toString());// sat apr 20 2024 14:11:56
 //console.log(myDate.toDateString());// sat apr 20 2024
 //console.log(myDate.toLocaleString());// 4/20/2024 2:11:56PM
 //console.log(typeof myDate); // object
 
-let myCreatedDate = new Date("2023-01-14") // mon jan 23 2023
+let myCreatedDate = new Date[2023,0,23] ;// mon jan 23 2023
+let myCreatedDate1 = new Date[2023,0,23] ;// mon jan 23 2023 5:03 am
+let myCreatedDate2 = new Date("2023-01-15");
 //console.log(myCreatedDate.toDateString());
 
 
 let myTimeStamp=Date.now();
-console.log(myTimeStamp);
+console.log(myTimeStamp);//89897878
 console.log(myCreatedDate.getTime());
 console.log(Math.floor(Date.now()/1000));
 
@@ -21,3 +25,5 @@ newDate.toLocaleString('default',{
     weekday:"long",
 });
 
+
+// done>>>>>>>>>>>>>>>
