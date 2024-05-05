3e3200a5ab97622468876cf32bbacaba11d354f8
Shubham singh <142674109+SHUBham66ingh@users.noreply.github.com>
2024-05-05 11:05:25 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -29,13 +29,13 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3)// it does not change the original array....
 
 console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3) // it does change th original array.....
 console.log("C ", myArr);
 console.log(myn2);
 
